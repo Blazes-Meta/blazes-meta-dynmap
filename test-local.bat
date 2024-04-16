2e9c5a118649a3fc6b88e126c06cb2cfc987d390
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-04-16 20:16:58 +0200

    Sync

--- test-local.bat
+++ test-local.bat
@@ -4,3 +4,4 @@ start cmd /k "cd server && npm install vite && npm run dev"
 start cmd /k "cd client && npm install express && npm run dev"
 
 cloudflared --url http://localhost:5173
+start "" "https://discord.com/developers/applications/1229430488528195624/embedded/url-mappings"
