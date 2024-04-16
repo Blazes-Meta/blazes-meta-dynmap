778d1efcaee7ded82e53942f7feb4a16a29d38bd
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-04-16 20:20:57 +0200

    Update test-local.bat

--- test-local.bat
+++ test-local.bat
@@ -1,7 +1,8 @@
 @echo off
+start "" "https://discord.com/developers/applications/1229430488528195624/embedded/url-mappings"
+
 start cmd /k "cd server && npm install vite && npm run dev"
 
 start cmd /k "cd client && npm install express && npm run dev"
 
 cloudflared --url http://localhost:5173
-start "" "https://discord.com/developers/applications/1229430488528195624/embedded/url-mappings"
