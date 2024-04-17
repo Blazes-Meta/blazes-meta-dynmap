357fe1aba188d0405d48385dc687cecf58e73742
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-04-17 19:36:20 +0200

    Update main.js

--- client/main.js
+++ client/main.js
@@ -5,7 +5,7 @@ document.querySelector('#app').innerHTML = `
   <div>
     
     <h1>Hello, World!</h1>
-    <iframe src="https://map.unlimitedworld.de" width="800" height="600" frameborder="0"></iframe>
+    <iframe src="/map" width="800" height="600" frameborder="0"></iframe>
   </div>
 `;
 
