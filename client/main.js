b035323c39317d357465c05488b23b332735bb3f
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-04-17 19:02:10 +0200

    Test

--- client/main.js
+++ client/main.js
@@ -1,9 +1,9 @@
-import './style.css'
-import rocketLogo from './rocket.png'
+//import './style.css'
+//import rocketLogo from './rocket.png'
 
 document.querySelector('#app').innerHTML = `
   <div>
-    <img src="${rocketLogo}" class="logo" alt="Discord" />
+    //<img src="${rocketLogo}" class="logo" alt="Discord" />
     <h1>Hello, World!</h1>
     <iframe src="https://map.unlimitedworld.de" width="800" height="600" frameborder="0"></iframe>
   </div>
