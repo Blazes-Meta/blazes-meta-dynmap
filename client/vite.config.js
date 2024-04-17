6623c580d7eb50e7cd113bc5a7d62105b9befc3b
Annhilati <78727320+annhilati@users.noreply.github.com>
2024-04-17 18:33:09 +0200

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite';
+import {defineConfig} from 'vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
   envDir: '../',
   server: {
-    port: 8080, // Hier ändern
+    //port: 8080, // Hier ändern
     proxy: {
       '/api': {
         target: 'http://localhost:3001',
