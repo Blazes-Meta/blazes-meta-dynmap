ad68bc6ef8a8563b4259e37e339fde94b77560e6
Annhilati <antbe.artis@web.de>
2024-04-20 16:32:19 +0200

    Sync

--- client/main.js
+++ client/main.js
@@ -1,13 +1,25 @@
 //import './style.css'
 //import rocketLogo from './rocket.png'
 
-document.querySelector('#app').innerHTML = `
-  <div>
+// main.js
+
+// Hier wird der Inhalt der HTML-Datei in die Webseite eingefügt
+fetch('/index.html')
+  .then(response => response.text())
+  .then(html => {
+    document.querySelector('#app').innerHTML = html;
+  })
+  .catch(error => {
+    console.error('Fehler beim Laden der HTML-Datei:', error);
+  });
+
+// document.querySelector('#app').innerHTML = `
+//   <div>
     
-    <h1>Hello, World!</h1>
-    <p>test js</p>
-    <iframe id="frame" src="/map"></iframe>
-  </div>
-`;
+//     <h1>Hello, World!</h1>
+//     <p>test js</p>
+//     <iframe id="frame" src="/map"></iframe>
+//   </div>
+// `;
 
 //<img src="${rocketLogo}" class="logo" alt="Discord" />
